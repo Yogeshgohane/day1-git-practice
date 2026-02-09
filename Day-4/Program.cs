48d9835b1a79cb6145360e85fd08faecfde08756
Yogesh Gohane <yogeshgohane2021@gmail.com>
2026-02-09 20:14:12 +0530

    Updated work

--- Day-4/Program.cs
+++ Day-4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿// See https://aka.ms/new-console-template for more information
+//Method poverloading
+using System;
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        int intResult = calc.Add(10, 20);
+        Console.WriteLine("Int Add Result: " + intResult);
+
+        double doubleResult = calc.Add(10.5, 20.3);
+        Console.WriteLine("Double Add Result: " + doubleResult);
+    }
+}
