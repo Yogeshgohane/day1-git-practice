2a959047ff7aad5025994a419e565cf7a6e36700
Yogesh Gohane <yogeshgohane2021@gmail.com>
2026-02-17 10:57:31 +0530

    Ignore bin and obj folders and clean repo

---
+++ Day-7/Day-7.2/MyDay72App/LinqDemo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+    }
+
+    class LinqDemoApp
+    {
+        public void Run()
+        {
+            var students = new List<Student>
+            {
+                new Student { Name = "Alice", Age = 20, Grade = "A" },
+                new Student { Name = "Bob", Age = 22, Grade = "B" },
+                new Student { Name = "Charlie", Age = 21, Grade = "A" },
+                new Student { Name = "David", Age = 23, Grade = "C" },
+                new Student { Name = "Eve", Age = 20, Grade = "B" }
+            };
+
+            var olderStudents =
+                from s in students
+                where s.Age > 21
+                select s;
+
+            foreach (var student in olderStudents)
+            {
+                Console.WriteLine($"{student.Name} is {student.Age} years old.");
+            }
+        }
+    }
+}
---
+++ Day-9/AdoDotnetAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Connection String
+            string connStr =
+            "server=localhost;database=customerdb;user=root;password=7719863765;";
+
+            using var conn = new MySqlConnection(connStr);
+
+            Console.WriteLine("Connected Successfully");
+            // SQL Query
+            string query = "SELECT * FROM Customer";
+
+            // Create Adapter
+            MySqlDataAdapter adapter =
+                new MySqlDataAdapter(query, conn);
+
+            // Create DataSet
+            DataSet ds = new DataSet();
+
+            // Fill DataSet
+            adapter.Fill(ds, "CustomerTable");
+
+            // Read Data
+            Console.WriteLine("\nCustomer List:\n");
+
+            foreach (DataRow row in ds.Tables["CustomerTable"].Rows)
+            {
+                Console.WriteLine(
+                    $"ID: {row["CustomerId"]} | Name: {row["CustomerName"]}"
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
